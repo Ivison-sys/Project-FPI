d500723d0a89534b0a6f2e445c21f0ff7abaeb80
gitiago4 <ils4@.cin.ufpe.br>
2025-07-29 10:17:22 -0300

    Teste de alteraçao

--- Hoquei/gameHoquei.h
+++ Hoquei/gameHoquei.h
@@ -2,6 +2,23 @@
 #define GAMEHOQUEI_H
 
 #include "../lib/raylib.h"
+
+typedef struct Jogador{
+    Vector2 posicao;
+    Vector2 velocidade;
+    Vector2 iniciogol;
+    int gols = 0;
+}Jogador;
+
+typedef struct Bola{
+    Vector2 posicao;
+    Vector2 velocidade;
+
+
+
+}
+
+
 void gameHoquei();
 
 #endif
