2c5b2893d06516608018c40e37fe166857aa24a8
ErickBarros <99843834+ErickBarros123@users.noreply.github.com>
2025-08-13 07:45:03 -0300

    refatoração

--- Golf/main.c
+++ Golf/main.c
@@ -2,10 +2,9 @@
 
 int main(void)
 {
-    // A função gameGolf() agora gerencia todo o ciclo de vida do jogo.
-    // Ela vai criar a janela, rodar o loop principal e liberar os recursos no final.
+    
     gameGolf();
 
-    // Nada mais é necessário aqui.
+    
     return 0;
 }
